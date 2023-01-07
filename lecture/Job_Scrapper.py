77410516a2fda997ab161f9bab27e0fbf5fe8cdb
yun <austru00@gmail.com>
2023-01-07 14:42:52 +0900

    5.7 Job Extraction

--- lecture/Job_Scrapper.py
+++ lecture/Job_Scrapper.py
@@ -14,5 +14,11 @@ else:
         job_posts = job_section.find_all('li')
         job_posts.pop(-1)
         for post in job_posts:
-            print(post)
-            print("///////")
+            anchors = post.find_all('a')
+            anchor = anchors[1]
+            link=anchor['href']
+            company,kind,region=anchor.find_all('span',class_='company')
+            title = anchor.find('span',class_='title')
+            print(company,kind,region,title)
+            print("///////////")
+            
