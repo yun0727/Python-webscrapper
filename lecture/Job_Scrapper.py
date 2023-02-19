ca1c5e951b5630cfbc9e0b147318a8e62463154d
yun <austru00@gmail.com>
2023-02-19 13:04:35 +0900

    5_3_initial_request

--- lecture/Job_Scrapper.py
+++ lecture/Job_Scrapper.py
@@ -1,5 +1,4 @@
 from requests import get
-from bs4 import BeautifulSoup
 
 base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
 search_term = "python"
@@ -8,18 +7,4 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website:")
 else:
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs=soup.find_all('section', class_="jobs")
-    for job_section in jobs:
-        
-        job_posts = job_section.find_all('li')
-        job_posts.pop(-1)
-        for post in job_posts:
-            anchors = post.find_all('a')
-            anchor = anchors[1]
-            link=anchor['href']
-            company,kind,region=anchor.find_all('span',class_='company')
-            title = anchor.find('span',class_='title')
-            print(company,kind,region,title)
-            print("///////////")
-            
+    print(response.text)
