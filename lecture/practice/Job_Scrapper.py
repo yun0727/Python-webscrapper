9b67b102aff3bb1a0b9b382d2347299f01ba5cec
yun <austru00@gmail.com>
2023-02-19 15:36:40 +0900

    5_13_None

--- lecture/practice/Job_Scrapper.py
+++ lecture/practice/Job_Scrapper.py
@@ -1,6 +1,22 @@
 from requests import get
+from selenium import webdriver
 from bs4 import BeautifulSoup
-from extractors.wwr import extract_wwr_jobs
 
-jobs=extract_wwr_jobs("python")
-print(jobs)
+browser = webdriver.Chrome()
+browser.get("https://kr.indeed.com/jobs?q=python&limit=50")
+soup=BeautifulSoup(browser.page_source,'html.parser')
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+
+jobs = job_list.find_all('li', recursive=False)
+
+for job in jobs:
+    zone=job.find("div",class_='mosaic-zone')
+    if zone == None:
+        print('job li')
+    else:
+        print('mosaic li')
+    
+while (True):
+    pass
+
+
