7f54eae83065817bfbeb47f04015bebe8534d50b
yun <austru00@gmail.com>
2023-02-19 13:08:05 +0900

    5_6_Job_Posts

--- lecture/Job_Scrapper.py
+++ lecture/Job_Scrapper.py
@@ -9,4 +9,10 @@ if response.status_code != 200:
     print("Can't request website:")
 else:
     soup = BeautifulSoup(response.text, "html.parser")
-    job=soup.find_all('section', class_="jobs")
+    jobs=soup.find_all('section', class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            print(post)
+            print("///////")
